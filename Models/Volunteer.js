b8ae16c5019133bb2106c5a683b89656d5a866cc
goshroma <roma.ghosh.02@gmail.com>
2024-06-27 20:12:31 +0530

    Volunteer model schema added

---
+++ Models/Volunteer.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const volunteerSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  location: {
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    country: {
+      type: String,
+      required: true,
+    },
+  },
+  preferences: {
+    skills: {
+      type: [String],
+      required: true
+    },
+    causes: {
+         type: [String],
+         type: [String],
+         required: true,
+         validate: {
+           validator: (v) => v.length >= 2,
+           message: 'Please select at least two causes',
+         },
+    },
+  },
+});
+
+module.exports = mongoose.model('Volunteer', volunteerSchema);
